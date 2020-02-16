8b9a5510a17a2cd72ebee433e174d51ec1967553
chenyunli <m18301662790@gmail.com>
2020-02-16 22:45:46 +0800

    ajax select  tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update!( post_params )
+  
+    render :json => { :id => @post.id, :message => "ok"}
+  end
+
   def create
     @post = Post.new(post_params)
     @post.user = current_user
@@ -58,6 +65,6 @@ class PostsController < ApplicationController
   protected
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :category_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_141138) do
+ActiveRecord::Schema.define(version: 2020_02_16_143454) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_02_16_141138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "flag_at"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
